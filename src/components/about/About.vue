9497233c0c5ba8fcc338c4c1b577cf3cf9262e08
zengpinru <1947383077@qq.com>
2018-08-27 18:04:24 +0800

    add SubNav component

--- src/components/about/About.vue
+++ src/components/about/About.vue
@@ -1,14 +1,17 @@
 <template>
   <main>
     <met-banner/>
+    <sub-nav />
   </main>
 </template>
 
 <script>
 import MetBanner from '@/components/global/MetBanner'
+import SubNav from '@/components/global/SubNav'
 export default {
   components: {
-    MetBanner
+    MetBanner,
+    SubNav
   }
 }
 </script>
---
+++ src/components/global/SubNav.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="sub-nav">
+    <div class="container clearfix">
+      <div class="left">
+        <a href="#" class="active">- 公司简介</a>
+        <a href="#">- 古筝文化</a>
+      </div>
+      <div class="right"></div>
+    </div>
+  </div>
+</template>
+
+<style>
+.sub-nav {
+  width: 100%;
+  height: 50px;
+  background: #b8b6b6;
+}
+
+.container {
+  width: 90%;
+  margin: 0 auto;
+}
+
+.left {
+  float: left;
+  line-height: 50px;
+  vertical-align: middle;
+}
+
+.left a {
+  display: inline-block;
+  height: 30px;
+  padding: 0 10px;
+  border-radius: 3px;
+  line-height: 30px;
+  font-size: 13px;
+  color: #ffffff;
+}
+
+.left a.active {
+  color: #b8b6b6;
+  background: #ffffff;
+}
+
+.right {
+  float: right;
+}
+</style>
